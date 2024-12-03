d07bb5ed54ba503186f5c827c5e5becfc9b31fe2
artikgak <artem.hak@ukma.edu.ua>
2024-12-03 23:25:05 +0200

    refactored case into lambda

--- Task3.hs
+++ Task3.hs
@@ -12,8 +12,9 @@ main = do
     contents <- hGetContents handle
     let 
         splitted = map (splitOn ",") .  concatMap (splitOn ")") . splitOn "mul("
-        filtered = filter (\x -> (length x == 2) && (all isDigit $ head x) && (all isDigit $ last x))
-        readAndCalc = foldl1 (+) . map (\[x,y] -> if null x || null y then 0 else (read x :: Int) * (read y :: Int))
+        --filtered = filter (\x -> (length x == 2) && (all isDigit $ head x) && (all isDigit $ last x) && (not . null $ head x) && (not . null $ last x))
+        filtered = filter (\x -> case x of {[a,b] -> (all isDigit a) && (all isDigit b) && (not $ null a) && (not $ null b); _ -> False})
+        readAndCalc = foldl1 (+) . map (\[x,y] -> (read x :: Int) * (read y :: Int))
         res1 = (readAndCalc . filtered . splitted) contents
         res2 = (readAndCalc . filtered . splitted . getEnabledParts) contents
     --print $ "Input: " ++ contents
